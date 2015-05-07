86afd8c8abd7238892c91a9cb507c5bea955de9f
Alessandro Iori <alessandro.iori92@gmail.com>
2015-05-07 08:35:15 +0200

    Status icons + functions of optimization

--- MacMenubarApp/AppDelegate.swift
+++ MacMenubarApp/AppDelegate.swift
@@ -2,15 +2,9 @@
 //  AppDelegate.swift
 //  MacMenubarApp
 //
-//  Created by Alessandro on 03/05/15.
-//  Copyright (c) 2015 Alessandro Iori. All rights reserved.
+//  Created by Alessandro Iori on 03/05/15.
 //
-//
-//  AppDelegate.swift
-//  MenuBarApp
-//
-//  Created by Alessandro on 03/05/15.
-//  Copyright (c) 2015 Alessandro Iori. All rights reserved.
+//  https://github.com/alessandroiori
 //
 
 import Cocoa
@@ -22,37 +16,51 @@ class AppDelegate: NSObject, NSApplicationDelegate {
     @IBOutlet weak var statusMenu: NSMenu!
     
     let statusItem = NSStatusBar.systemStatusBar().statusItemWithLength(-1)
+    let icon1 = NSImage(named: "statusIcon1")
+    let icon2 = NSImage(named: "statusIcon2")
     
-    func applicationDidFinishLaunching(aNotification: NSNotification) {
-        let icon = NSImage(named: "statusIcon")
-        icon?.setTemplate(true)
-        
-        statusItem.image = icon
-        statusItem.menu = statusMenu
-    }
-    
-    @IBAction func menuClicked(sender: NSMenuItem) {
-        let task = NSTask()
-        task.launchPath = "/usr/bin/defaults"
-        
-        if(sender.state == NSOnState){
-            sender.state = NSOffState
-            task.arguments = ["write", "com.apple.finder", "AppleShowAllFiles", "NO"]
-        } else {
-            sender.state = NSOnState
-            task.arguments = ["write", "com.apple.finder", "AppleShowAllFiles", "YES"]
-        }
-        
-        task.launch()
-        task.waitUntilExit()
-        
+    func KillAllFinder(){
         let killtask = NSTask()
         killtask.launchPath = "/usr/bin/killall"
         killtask.arguments = ["Finder"]
         killtask.launch()
     }
     
+    func FinderShow(YN: String){
+        let task = NSTask()
+        task.launchPath = "/usr/bin/defaults"
+        task.arguments = ["write", "com.apple.finder", "AppleShowAllFiles", YN]
+        task.launch()
+        task.waitUntilExit()
+    }
+    
+    func applicationDidFinishLaunching(aNotification: NSNotification) {
+        icon1?.setTemplate(true)
+        icon2?.setTemplate(true)
+        
+        statusItem.image = icon1
+        statusItem.menu = statusMenu
+    }
+    
+    @IBAction func menuClicked(sender: NSMenuItem) {
+        
+        if(sender.state == NSOnState){
+            statusItem.image = icon1
+            sender.state = NSOffState
+            FinderShow("NO")
+        } else {
+            statusItem.image = icon2
+            sender.state = NSOnState
+            FinderShow("YES")
+        }
+        
+        KillAllFinder()
+    }
+    
     @IBAction func menuQuitClicked(sender: AnyObject) {
+        FinderShow("NO")
+        KillAllFinder()
+        
         NSApplication.sharedApplication().terminate(self)
     }
 }
